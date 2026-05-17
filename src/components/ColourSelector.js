7f956b4d86f7c2dc0c9ad780be27052ead4c354e
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-05-17 21:41:23 +0530

    Add key to ColourSelector props and update button

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
 const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props
-  const { background, classname, label } = config;
+  const { config, selectNextBackground } = props;
+
+  const { background, classname, label, key } = config;
 
   return (
     <button
+      data-testid={key}
       className={classname}
       onClick={() => selectNextBackground({ background: background })}
     >
       {label}
     </button>
-  )
-}
+  );
+};
 
 export default ColourSelector;
