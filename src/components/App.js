fe5f57d16e372b250127750ef5363fa0e694f649
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2026-05-17 21:36:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -25,17 +25,22 @@ const title = 'Select the gradient and then the Box to change the color';
 
 const App = () => {
   let [nextBackground, selectNextBackground] = useState({ background: "" })
+
   const applyColor = (updateSelectionStyle) => {
     updateSelectionStyle(nextBackground)
   }
 
   return (
     <div id="master">
-      <h5 className="heading">{/* display title here */}</h5>
+      <h5 className="heading">{title}</h5>
 
       <div className="row">
-        {colourConfig.map((config, index) => (
-          <ColourSelector key={config.key} config={config} selectNextBackground={selectNextBackground} />
+        {colourConfig.map((config) => (
+          <ColourSelector
+            key={config.key}
+            config={config}
+            selectNextBackground={selectNextBackground}
+          />
         ))}
       </div>
 
@@ -49,6 +54,5 @@ const App = () => {
     </div >
   )
 }
-
 
 export default App;
